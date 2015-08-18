5a40fad1d257cd3a17266fe107b0d943b9082e15
Andreas Henelius <andreas.henelius@ttl.fi>
2015-08-18 12:47:08 +0300

    Improved installation via PIP.

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ setup(name='midas',
       description='The MIDAS (Modular Integrated Distributed Analysis System) is a system for online analysis of streaming signals and allows easy integration of such into machine learning frameworks.',
       author='Andreas Henelius, Jari Torniainen, Brain Work Research Center at the Finnish Institute of Occupational Health',
       author_email='andreas.henelius@ttl.fi, jari.torniainen@ttl.fi',
-      url='http://github.io/FIOH-BWRC/midas',
+      url='https://github.com/bwrc/midas',
       license='MIT',
       packages=['midas'],
       package_dir={'midas': 'midas'},
-      include_package_data=False)
+      include_package_data=False,
+      install_requires = ['bottle>=0.12',
+                          'PyZMQ>=14.3.1',
+                          'Waitress>=0.8.9',
+                          'pylsl>=1.10.4'],
+      entry_points={"console_scripts":
+                    ["midas-dispatcher = midas.dispatcher:run_from_cli"]}
+)
